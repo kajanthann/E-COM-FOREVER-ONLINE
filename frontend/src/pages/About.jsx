31b7142971d8fba68ce8226c622a478b8b17ea23
kajanthann <arulkajanthan904@gmail.com>
2025-06-14 12:41:50 +0530

    ..

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -8,6 +8,7 @@ const About = () => {
     <div>
     <div className='text-2xl text-center pt-8 border-t'>
       <Title text1={'ABOUT'} text2={'US'} />
+      
     </div>
     <div className='my-10 flex flex-col md:flex-row gap-16'>
       <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
